26dd7723f3fb268c9b801be2574abd2bf576b199
Ching-Yu Yang <ching-yu@Ching-Yude-MacBook-Pro.local>
2018-04-24 11:11:57 +0800

    first commit

---
+++ wordcount_cy.py
@@ -0,0 +1,49 @@
+import fileinput
+import string
+from collections import Counter
+import csv
+import json
+import pickle
+
+
+def to_word(words):
+    line_words = []
+    for word in words:
+        if word:
+            for i in range(len(word)):
+                if word[-1] in string.punctuation:
+                    word = word[:-1]  
+                elif word[0] in string.punctuation:
+                    word = word[1:]      
+                else:
+                    break
+            if word:
+                line_words.append(word)
+    return line_words
+
+
+def main():
+    all_words = []
+    for line in fileinput.input():
+        line_words = to_word(line.strip().split(' '))
+        all_words.extend(line_words)
+
+     
+    counter = Counter(all_words)
+
+    with open('wordcount.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['word', 'count'])
+        for counting in counter.most_common():
+            writer.writerow(counting)
+    
+    json.dump(counter.most_common(), open('wordcount.json', 'w'))
+    
+    pickle.dump(counter, open('wordcount.pkl', 'wb'))
+
+
+
+if __name__ == '__main__':
+    main()
+
+
